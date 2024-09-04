ef82cda6657c207c9756f464f852b38833790f20
David_Chen_HT <vscode1005@hotaileasing.com.tw>
2024-09-04 11:40:46 +0800

    first commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Breakfast',
+          style: TextStyle(
+              color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        centerTitle: true,
+        leading: Container(
+          alignment: Alignment.center, //這裡是因應bug, 不然底下SvgPicture的寬高設定無效
+          margin: EdgeInsets.all(10),
+          child: SvgPicture.asset(
+            'assets/icons/Arrow - Left 2.svg',
+            height: 20,
+            width: 20,
+          ),
+          decoration: BoxDecoration(
+              color: Color(0xffF7F8F8),
+              borderRadius: BorderRadius.circular(10)),
+        ),
+        actions: [
+          Container(
+            margin: EdgeInsets.all(10),
+            alignment: Alignment.center, //這裡是因應bug, 不然底下SvgPicture的寬高設定無效
+            width: 37,
+            child: SvgPicture.asset(
+              'assets/icons/dots.svg',
+              height: 5,
+              width: 5,
+            ),
+            decoration: BoxDecoration(
+                color: Color(0xffF7F8F8),
+                borderRadius: BorderRadius.circular(10)),
+          ),
+        ],
+      ),
+    );
+  }
+}
